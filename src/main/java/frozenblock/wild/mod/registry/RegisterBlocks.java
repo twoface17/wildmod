c9dbb59abd6447dc8a9fe6c62893d2f90abcc7fb
AlexTheDolphim0 <79395212+AlexTheDolphin0@users.noreply.github.com>
2021-11-03 23:17:15 -0500

    - Fixed net.minecraft.block.Blocks not being imported into RegisterBlocks.java(Sorry for not making this and the previous 1 commit. Didn't notice this one at first)

--- src/main/java/frozenblock/wild/mod/registry/RegisterBlocks.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterBlocks.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
