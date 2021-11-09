b92ed21541f07eb0572fa5b11052d47b5734c453
BluePhoenixLOL <XfrPlayz@gmail.com>
2021-11-09 11:31:33 -0700

    - Warden Slightly angry Sound Instead of Shriek when death - instead of "twm:shrieker" it's "twm:block.shrieker.shriek" - instead of "twm:slightly_angry" it's "twm:entity.warden.slightly_angry"

--- src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
@@ -7,10 +7,10 @@ import net.minecraft.util.registry.Registry;
 
 public class RegisterSounds {
 
-    public static final Identifier SHRIEKER_ID = new Identifier(WildMod.MOD_ID, "shrieker");
+    public static final Identifier SHRIEKER_ID = new Identifier(WildMod.MOD_ID, "block.shrieker.shriek");
     public static SoundEvent SHRIEKER_EVENT = new SoundEvent(SHRIEKER_ID);
-    public static final Identifier SLIGHTLY_ANGRY_ID = new Identifier(WildMod.MOD_ID, "slightly_angry");
-    public static SoundEvent SLIGHTLY_ANGRY_EVENT = new SoundEvent(SHRIEKER_ID);
+    public static final Identifier SLIGHTLY_ANGRY_ID = new Identifier(WildMod.MOD_ID, "entity.warden.slightly_angry");
+    public static SoundEvent SLIGHTLY_ANGRY_EVENT = new SoundEvent(SLIGHTLY_ANGRY_ID);
 
     public static void RegisterSounds() {
         Registry.register(Registry.SOUND_EVENT, SHRIEKER_ID, SHRIEKER_EVENT);
