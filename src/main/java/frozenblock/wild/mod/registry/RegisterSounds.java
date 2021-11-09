0df61134ddf21074add2d67bb39ac4c48e715ae2
BluePhoenixLOL <XfrPlayz@gmail.com>
2021-11-09 11:16:31 -0700

    warden slightly angry sound

--- src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
@@ -9,8 +9,13 @@ public class RegisterSounds {
 
     public static final Identifier SHRIEKER_ID = new Identifier(WildMod.MOD_ID, "shrieker");
     public static SoundEvent SHRIEKER_EVENT = new SoundEvent(SHRIEKER_ID);
+    public static final Identifier SLIGHTLY_ANGRY_ID = new Identifier(WildMod.MOD_ID, "slightly_angry");
+    public static SoundEvent SLIGHTLY_ANGRY_EVENT = new SoundEvent(SHRIEKER_ID);
 
     public static void RegisterSounds() {
         Registry.register(Registry.SOUND_EVENT, SHRIEKER_ID, SHRIEKER_EVENT);
+        Registry.register(Registry.SOUND_EVENT, SLIGHTLY_ANGRY_ID, SLIGHTLY_ANGRY_EVENT);
     }
+
 }
+
