4c290e0e45de0350d2978e46c06c0574f674baaa
BluePhoenixLOL <XfrPlayz@gmail.com>
2021-11-09 12:17:43 -0700

    - Fixed Shriek Particle Causing Mipmapping to stop working - Shrieker particle now named "Shriek" - Shriek particles is now 16x16 - thiccer shriek particle with shadow - Fixed Mipmapping not working bug

--- src/main/java/frozenblock/wild/mod/registry/RegisterParticles.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterParticles.java
@@ -13,12 +13,12 @@ public abstract class RegisterParticles {
 
     // instance of our particle
     public static final DefaultParticleType FIREFLY = FabricParticleTypes.simple();
-    public static final DefaultParticleType SHRIEKER = FabricParticleTypes.simple();
+    public static final DefaultParticleType SHRIEK = FabricParticleTypes.simple();
     public static final DefaultParticleType SCULK_SOUL = FabricParticleTypes.simple();
 
     public static void RegisterParticles() {
         Registry.register(Registry.PARTICLE_TYPE, new Identifier(WildMod.MOD_ID, "firefly"), FIREFLY);
-        Registry.register(Registry.PARTICLE_TYPE, new Identifier(WildMod.MOD_ID, "shrieker"), SHRIEKER);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(WildMod.MOD_ID, "shriek"), SHRIEK);
         Registry.register(Registry.PARTICLE_TYPE, new Identifier(WildMod.MOD_ID, "sculk_soul"), SCULK_SOUL);
     }
 }
