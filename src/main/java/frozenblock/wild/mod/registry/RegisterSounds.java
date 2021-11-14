8729dbd0a04fa51077ad1a22e6f6f349aeb1434e
BluePhoenixLOL <XfrPlayz@gmail.com>
2021-11-14 13:31:42 -0700

    - Sculk Catalyst Sounds

--- src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterSounds.java
@@ -13,11 +13,20 @@ public class RegisterSounds {
     public static SoundEvent SLIGHTLY_ANGRY_EVENT = new SoundEvent(SLIGHTLY_ANGRY_ID);
     public static final Identifier WARDEN_AMBIENT_ID = new Identifier(WildMod.MOD_ID, "entity.warden.ambient");
     public static SoundEvent WARDEN_AMBIENT_EVENT = new SoundEvent(WARDEN_AMBIENT_ID);
+    public static final Identifier CATALYST_BREAK_ID = new Identifier(WildMod.MOD_ID, "block.sculk_catalyst.break");
+    public static SoundEvent CATALYST_BREAK_EVENT = new SoundEvent(CATALYST_BREAK_ID);
+    public static final Identifier CATALYST_PLACE_ID = new Identifier(WildMod.MOD_ID, "block.sculk_catalyst.place");
+    public static SoundEvent CATALYST_PLACE_EVENT = new SoundEvent(CATALYST_PLACE_ID);
+    public static final Identifier CATALYST_STEP_ID = new Identifier(WildMod.MOD_ID, "block.sculk_catalyst.step");
+    public static SoundEvent CATALYST_STEP_EVENT = new SoundEvent(CATALYST_STEP_ID);
 
     public static void RegisterSounds() {
         Registry.register(Registry.SOUND_EVENT, SHRIEKER_ID, SHRIEKER_EVENT);
         Registry.register(Registry.SOUND_EVENT, SLIGHTLY_ANGRY_ID, SLIGHTLY_ANGRY_EVENT);
         Registry.register(Registry.SOUND_EVENT, WARDEN_AMBIENT_ID, WARDEN_AMBIENT_EVENT);
+        Registry.register(Registry.SOUND_EVENT, CATALYST_BREAK_ID, CATALYST_BREAK_EVENT);
+        Registry.register(Registry.SOUND_EVENT, CATALYST_PLACE_ID, CATALYST_PLACE_EVENT);
+        Registry.register(Registry.SOUND_EVENT, CATALYST_STEP_ID,  CATALYST_STEP_EVENT);
     }
 
 }
