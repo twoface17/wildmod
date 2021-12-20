457afdc8acc8cc67082ac60f37fdb8caadde8cfb
Osmiooo <owencraft2000@gmail.com>
2021-12-20 16:02:36 +0100

    No collision for frog egg

--- src/main/java/frozenblock/wild/mod/blocks/FrogEggBlock.java
+++ src/main/java/frozenblock/wild/mod/blocks/FrogEggBlock.java
@@ -1,15 +1,11 @@
 package frozenblock.wild.mod.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
-import net.minecraft.block.PlantBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class FrogEggBlock
@@ -23,6 +19,11 @@ public class FrogEggBlock
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+            return VoxelShapes.empty();
     }
 
     @Override
