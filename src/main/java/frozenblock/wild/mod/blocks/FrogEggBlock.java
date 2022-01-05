49702ca8c5f73f42f6ddc118155a74bf383ee1ab
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-05 18:22:18 -0500

    Frog Egg hatching
    Spawns 1-6 Tadpoles upon hatching like the Bedrock Betas.

--- src/main/java/frozenblock/wild/mod/blocks/FrogEggBlock.java
+++ src/main/java/frozenblock/wild/mod/blocks/FrogEggBlock.java
@@ -1,7 +1,11 @@
 package frozenblock.wild.mod.blocks;
 
+import frozenblock.wild.mod.entity.TadpoleEntity;
+import frozenblock.wild.mod.registry.RegisterEntities;
 import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.SchoolingFishEntity;
 import net.minecraft.entity.passive.TurtleEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
@@ -11,7 +15,9 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -40,11 +46,28 @@ public class FrogEggBlock
         return SHAPE;
     }
 
-
-
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
             return VoxelShapes.empty();
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        super.randomTick(state, world, pos, random);
+        if (UniformIntProvider.create(1,4).get(world.getRandom())==4) {
+            int tadpoles = UniformIntProvider.create(1, 6).get(world.getRandom());
+            for (int t = 0; t < tadpoles; t++) {
+                TadpoleEntity tadpoleEntity = (TadpoleEntity) RegisterEntities.TADPOLE.create(world);
+                tadpoleEntity.refreshPositionAndAngles((double) pos.getX() + 0.3D + 0.2D, (double) pos.getY(), (double) pos.getZ() + 0.3D, 0.0F, 0.0F);
+                world.spawnEntity(tadpoleEntity);
+            }
+            world.breakBlock(pos, false);
+        }
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        return true;
     }
 
     @Override
@@ -52,6 +75,10 @@ public class FrogEggBlock
         FluidState fluidState = world.getFluidState(pos);
         FluidState fluidState2 = world.getFluidState(pos.up());
         return (fluidState.getFluid() == Fluids.WATER && fluidState2.getFluid() == Fluids.EMPTY);
+    }
+
+    public static boolean isWater(BlockView world, BlockPos pos) {
+        return world.getBlockState(pos)==Blocks.WATER.getDefaultState();
     }
 
 }
