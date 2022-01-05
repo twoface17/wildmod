c20e122120a2ca1f6eba7df0d7d00b07b6c72752
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-05 16:27:18 -0500

    Move Vibrations To Entity& No Vibration/Sound Spam
    Moves vibration handling to the entity itself to make the code less cluttered (sorry about cluttering it initially)
    Is much more reliable than my old PR that I closed as it uses the Frog's timing system
    Update .playSound to match the Sculk Sensor's pitch-choosing code
    Uses same cooldown as Sculk Sensor (20 ticks for Sensor being activated, 3 for its cooldown)

--- src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
@@ -11,9 +11,12 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.*;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Vibration;
 import net.minecraft.world.World;
 import net.minecraft.world.event.PositionSource;
 import net.minecraft.world.event.PositionSourceType;
@@ -33,6 +36,10 @@ public class WardenEntity extends HostileEntity {
     public BlockPos lasteventpos;
     public World lasteventworld;
     public LivingEntity lastevententity;
+
+    public long vibrationTimer = 0;
+    protected int delay = 0;
+    protected int distance;
 
 
     public WardenEntity(EntityType<? extends HostileEntity> entityType, World world) {
@@ -114,11 +121,38 @@ public class WardenEntity extends HostileEntity {
     protected SoundEvent getAmbientSound(){return RegisterSounds.ENTITY_WARDEN_AMBIENT;}
 
     public void listen(BlockPos eventPos, World eventWorld, LivingEntity eventEntity) {
-        if(this.lasteventpos == eventPos && this.lasteventworld == eventWorld && this.lastevententity == eventEntity) {
-            this.playSound(SoundEvents.BLOCK_SCULK_SENSOR_CLICKING, 0.5F, 1.0F);
-        }
+        if(this.lasteventpos == eventPos && this.lasteventworld == eventWorld && this.lastevententity == eventEntity && this.world.getTime()-this.vibrationTimer>=23) {
+            this.vibrationTimer=this.world.getTime();
+            this.playSound(SoundEvents.BLOCK_SCULK_SENSOR_CLICKING, 0.5F, world.random.nextFloat() * 0.2F + 0.8F);
+            BlockPos WardenHead = this.getBlockPos().up((int) 3);
+            PositionSource wardenPositionSource = new PositionSource() {
+                @Override
+                public Optional<BlockPos> getPos(World world) {
+                    return Optional.of(WardenHead);
+                }
+
+                @Override
+                public PositionSourceType<?> getType() {
+                    return null;
+                }
+            };
+                CreateVibration(this.world, lasteventpos, wardenPositionSource, WardenHead);
+            }
         this.lasteventpos = eventPos;
         this.lasteventworld = eventWorld;
         this.lastevententity = eventEntity;
+    }
+
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putLong("vibrationTimer", this.vibrationTimer);
+    }
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.vibrationTimer = nbt.getLong("vibrationTimer");
+    }
+    public void CreateVibration(World world, BlockPos blockPos, PositionSource positionSource, BlockPos blockPos2) {
+        this.delay = this.distance = (int)Math.floor(Math.sqrt(blockPos.getSquaredDistance(blockPos2, false))) * 2 ;
+        ((ServerWorld)world).sendVibrationPacket(new Vibration(blockPos, positionSource, this.delay));
     }
 }
