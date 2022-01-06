e35807b4ec4087f2fffe18aea18ff0d14ef1a96a
AlexTheDolphim0 <79395212+AlexTheDolphin0@users.noreply.github.com>
2022-01-05 23:01:07 -0600

    Fixed dirt being gone

--- src/main/java/frozenblock/wild/mod/mixins/VanillaSurfaceRulesInjector.java
+++ src/main/java/frozenblock/wild/mod/mixins/VanillaSurfaceRulesInjector.java
@@ -4,12 +4,16 @@ import com.google.common.collect.ImmutableList;
 import frozenblock.wild.mod.registry.RegisterBlocks;
 import frozenblock.wild.mod.registry.RegisterWorldgen;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.math.VerticalSurfaceType;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.noise.NoiseParametersKeys;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
 import net.minecraft.world.gen.surfacebuilder.VanillaSurfaceRules;
+import org.lwjgl.system.CallbackI;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -17,9 +21,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(net.minecraft.world.gen.surfacebuilder.VanillaSurfaceRules.class)
 public class VanillaSurfaceRulesInjector {
+    @Shadow @Final private static MaterialRules.MaterialRule DIRT;
+
     @ModifyVariable(method = "createDefaultRule", at = @At("STORE"), ordinal = 8)
     private static MaterialRules.MaterialRule injected(MaterialRules.MaterialRule x) {
-        return MaterialRules.sequence(MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(RegisterWorldgen.MANGROVE_SWAMP), VanillaSurfaceRulesBlockInvoker.invokeBlock(RegisterBlocks.MUD_BLOCK)), MaterialRules.condition(MaterialRules.biome(BiomeKeys.SWAMP), MaterialRules.condition(MaterialRules.aboveY(YOffset.fixed(62), 0), MaterialRules.condition(MaterialRules.not(MaterialRules.aboveY(YOffset.fixed(63), 0)), MaterialRules.condition(MaterialRules.noiseThreshold(NoiseParametersKeys.SURFACE_SWAMP, 0.0), VanillaSurfaceRulesBlockInvoker.invokeBlock(Blocks.WATER))))), x)));
+        return MaterialRules.sequence(MaterialRules.condition(
+                MaterialRules.STONE_DEPTH_FLOOR, MaterialRules.sequence(
+                        MaterialRules.condition(
+                                MaterialRules.biome(
+                                        RegisterWorldgen.MANGROVE_SWAMP),
+                                VanillaSurfaceRulesBlockInvoker.invokeBlock(
+                                        RegisterBlocks.MUD_BLOCK
+                                )
+                        ),
+                        MaterialRules.condition(
+                                MaterialRules.biome(
+                                        RegisterWorldgen.MANGROVE_SWAMP),
+                                MaterialRules.condition(
+                                        MaterialRules.aboveY(YOffset.fixed(62), 0),
+                                        MaterialRules.condition(
+                                                MaterialRules.not(
+                                                        MaterialRules.aboveY(YOffset.fixed(63), 0)),
+                                                MaterialRules.condition(
+                                                        MaterialRules.noiseThreshold(
+                                                                NoiseParametersKeys.SURFACE_SWAMP, 0.1),
+                                                        VanillaSurfaceRulesBlockInvoker.invokeBlock(Blocks.WATER))))))),
+                x);
     }
-
 }
