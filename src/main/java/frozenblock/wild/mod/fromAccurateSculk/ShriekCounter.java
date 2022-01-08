cac9bbb17989ecbca1c6457f6253504c61dedf80
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-08 15:42:34 -0500

    Forgot To Add Particles When Summoned By Shrieker

--- src/main/java/frozenblock/wild/mod/fromAccurateSculk/ShriekCounter.java
+++ src/main/java/frozenblock/wild/mod/fromAccurateSculk/ShriekCounter.java
@@ -42,6 +42,7 @@ public class ShriekCounter {
                                 WardenEntity warden = (WardenEntity) RegisterEntities.WARDEN.create(world);
                                 warden.refreshPositionAndAngles((double) currentCheck.getX() + 1D, (double) currentCheck.up(1).getY(), (double) currentCheck.getZ() + 1D, 0.0F, 0.0F);
                                 world.spawnEntity(warden);
+                                warden.handleStatus((byte) 5);
                                 world.playSound(null, currentCheck, RegisterSounds.ENTITY_WARDEN_EMERGE, SoundCategory.HOSTILE, 1F, 1F);
                                 break;
                             }
