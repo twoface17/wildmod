2c7b9387ea7c3972de0611ae74957365ce0e2105
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-08 15:46:30 -0500

    Shrieker Summoning Works With Previous Commit

--- src/main/java/frozenblock/wild/mod/fromAccurateSculk/ShriekCounter.java
+++ src/main/java/frozenblock/wild/mod/fromAccurateSculk/ShriekCounter.java
@@ -43,6 +43,7 @@ public class ShriekCounter {
                                 warden.refreshPositionAndAngles((double) currentCheck.getX() + 1D, (double) currentCheck.up(1).getY(), (double) currentCheck.getZ() + 1D, 0.0F, 0.0F);
                                 world.spawnEntity(warden);
                                 warden.handleStatus((byte) 5);
+                                warden.leaveTime=world.getTime()+1200;
                                 world.playSound(null, currentCheck, RegisterSounds.ENTITY_WARDEN_EMERGE, SoundCategory.HOSTILE, 1F, 1F);
                                 break;
                             }
