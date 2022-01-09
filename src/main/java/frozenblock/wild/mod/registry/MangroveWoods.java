a65dcea2bdf007e676c9bd67506675cf1fd7a5ef
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-08 22:31:46 -0600

    Update MangroveWoods.java

--- src/main/java/frozenblock/wild/mod/registry/MangroveWoods.java
+++ src/main/java/frozenblock/wild/mod/registry/MangroveWoods.java
@@ -32,7 +32,7 @@ public abstract class MangroveWoods {
     public static final PressurePlateBlock MANGROVE_PRESSURE_PLATE = new MangrovePressurePlate(PressurePlateBlock.ActivationRule.EVERYTHING, WOOD_PROPERTIES);
     public static final Block MANGROVE_TRAPDOOR = new MangroveTrapdoor(WOOD_PROPERTIES.nonOpaque());
     public static final Block MANGROVE_LEAVES = new MangroveLeaves();
-    public static final Block MANGROVE_ROOTS = new MangroveRoots(MANGROVE_ROOTS_SETTINGS.sounds(new BlockSoundGroup(1.0f, 1.0f,
+    public static final Block MANGROVE_ROOTS = new MangroveRoots(MANGROVE_ROOTS_SETTINGS.nonOpaque().sounds(new BlockSoundGroup(1.0f, 1.0f,
                 RegisterSounds.BLOCK_MANGROVE_ROOTS_BREAK,
                 RegisterSounds.BLOCK_MANGROVE_ROOTS_STEP,
                 RegisterSounds.BLOCK_MANGROVE_ROOTS_PLACE,
