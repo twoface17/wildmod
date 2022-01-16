e1db2f16ffb54df74b6b09d9ea1e80309b1d898f
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-16 12:59:44 -0600

    removed all gamerules (they don't have vanilla parallels so they dont make sense)

--- src/main/java/frozenblock/wild/mod/WildMod.java
+++ src/main/java/frozenblock/wild/mod/WildMod.java
@@ -24,30 +24,4 @@ public class WildMod implements ModInitializer {
         RegisterAccurateSculk.RegisterAccurateSculk();
     }
 
-    public static final GameRules.Key<GameRules.BooleanRule> DARKNESS_ENABLED =
-            GameRuleRegistry.register("doDarkness", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.BooleanRule> SHRIEKER_NEEDS_SCULK =
-            GameRuleRegistry.register("shriekerNeedsSculk", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> SHRIEKER_SHRIEKS =
-            GameRuleRegistry.register("shriekerShrieks", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.BooleanRule> DO_CATALYST_POLLUTION =
-            GameRuleRegistry.register("catalystPollution", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.BooleanRule> DO_CATALYST_VIBRATIONS =
-            GameRuleRegistry.register("catalystVibrations", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> CATALYST_DETECTS_ALL =
-            GameRuleRegistry.register("catalystDetectsAll", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> SHRIEKER_GARGLES =
-            GameRuleRegistry.register("shriekerGargles", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> SCULK_THREADING =
-            GameRuleRegistry.register("sculkThreading", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.IntRule> SCULK_MULTIPLIER =
-            GameRuleRegistry.register("spreadMultiplier", GameRules.Category.MISC, GameRuleFactory.createIntRule(1, 1, 20));
-    public static final GameRules.Key<GameRules.IntRule> SCULK_THREADS =
-            GameRuleRegistry.register("sculkThreads", GameRules.Category.MISC, GameRuleFactory.createIntRule(2, 1, 21));
-    public static final GameRules.Key<GameRules.IntRule> UPWARD_SPREAD =
-            GameRuleRegistry.register("upwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(3, 1, 100));
-    public static final GameRules.Key<GameRules.IntRule> DOWNWARD_SPREAD =
-            GameRuleRegistry.register("downwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(8, 1, 100));
-    public static final GameRules.Key<GameRules.BooleanRule> NO_WARDEN_COOLDOWN =
-            GameRuleRegistry.register("noWardenCooldown", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 }
