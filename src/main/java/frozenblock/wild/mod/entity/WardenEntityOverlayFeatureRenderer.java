d2daded2e3fbe78bb4cf2a63a7abf2fb047d8dd9
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-20 16:53:34 -0600

    added overlays textures for secret warden

--- src/main/java/frozenblock/wild/mod/entity/WardenEntityOverlayFeatureRenderer.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntityOverlayFeatureRenderer.java
@@ -6,17 +6,25 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class WardenEntityOverlayFeatureRenderer extends EyesFeatureRenderer<WardenEntity, WardenEntityModel> {
     private static final RenderLayer OVERLAY = RenderLayer.getEyes(new Identifier(WildMod.MOD_ID, "textures/entity/warden/warden_overlay.png"));
+    private static final RenderLayer SECRET_OVERLAY = RenderLayer.getEyes(new Identifier(WildMod.MOD_ID, "textures/entity/warden/secret_warden_overlay.png"));
+    private WardenEntity entity;
 
     public WardenEntityOverlayFeatureRenderer(FeatureRendererContext<WardenEntity, WardenEntityModel> featureRendererContext) {
         super(featureRendererContext);
     }
 
+    @Override
     public RenderLayer getEyesTexture() {
+        String string = Formatting.strip(entity.getName().getString());
+        if (string != null && "Osmiooo".equals(string)) {
+            return SECRET_OVERLAY;
+        }
         return OVERLAY;
     }
 }
