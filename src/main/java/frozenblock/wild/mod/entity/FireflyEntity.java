fb2c56187b93c225688b9be7c366cc3a3693779d
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-19 21:33:04 -0500

    Giant 20FPS Fireflies

--- src/main/java/frozenblock/wild/mod/entity/FireflyEntity.java
+++ src/main/java/frozenblock/wild/mod/entity/FireflyEntity.java
@@ -1,31 +1,151 @@
 package frozenblock.wild.mod.entity;
 
+import frozenblock.wild.mod.liukrastapi.FireflyWanderGoal;
 import frozenblock.wild.mod.registry.RegisterParticles;
+import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Flutterer;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.NoWaterTargeting;
+import net.minecraft.entity.ai.control.FlightMoveControl;
+import net.minecraft.entity.ai.control.LookControl;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.jetbrains.annotations.Nullable;
 
-public class FireflyEntity extends MobEntity {
-    public FireflyEntity(EntityType<? extends MobEntity> entityType, World world) {
+public class FireflyEntity extends AnimalEntity implements Flutterer {
+    public boolean spawnSet;
+    public int spawnX;
+    public int spawnY;
+    public int spawnZ;
+
+    public FireflyEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new FlightMoveControl(this, 20, true);
+        this.setPathfindingPenalty(PathNodeType.DANGER_FIRE, -1.0F);
+        this.setPathfindingPenalty(PathNodeType.WATER, -1.0F);
+        this.setPathfindingPenalty(PathNodeType.WATER_BORDER, 16.0F);
+        this.setPathfindingPenalty(PathNodeType.COCOA, -1.0F);
+        this.setPathfindingPenalty(PathNodeType.FENCE, -1.0F);
+    }
+
+    protected void initGoals() {
+        this.goalSelector.add(8, new FireflyWanderGoal(this));
+        this.goalSelector.add(7, new SwimGoal(this));
     }
 
     public static DefaultAttributeContainer.Builder createFireflyAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 10D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 10D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1).add(EntityAttributes.GENERIC_FLYING_SPEED, 1);
+    }
+
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    public float getPathfindingFavor(BlockPos pos, WorldView world) {
+        return world.getBlockState(pos).isAir() ? 10.0F : 0.0F;
+    }
+
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("spawnSet", this.spawnSet);
+        nbt.putInt("spawnX", this.spawnX);
+        nbt.putInt("spawnY", this.spawnY);
+        nbt.putInt("spawnZ", this.spawnZ);
+    }
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.spawnSet = nbt.getBoolean("spawnSet");
+        this.spawnX = nbt.getInt("spawnX");
+        this.spawnY = nbt.getInt("spawnY");
+        this.spawnZ = nbt.getInt("spawnZ");
+    }
+
+    public void startMovingTo(BlockPos pos) {
+        Vec3d vec3d = Vec3d.ofBottomCenter(pos);
+        int i = 0;
+        BlockPos blockPos = this.getBlockPos();
+        int j = (int)vec3d.y - blockPos.getY();
+        if (j > 2) {
+            i = 4;
+        } else if (j < -2) {
+            i = -4;
+        }
+
+        int k = 6;
+        int l = 8;
+        int m = blockPos.getManhattanDistance(pos);
+        if (m < 15) {
+            k = m / 2;
+            l = m / 2;
+        }
+
+        Vec3d vec3d2 = NoWaterTargeting.find(this, k, l, i, vec3d, 0.3141592741012573D);
+        if (vec3d2 != null) {
+            this.navigation.setRangeMultiplier(0.5F);
+            this.navigation.startMovingTo(vec3d2.x, vec3d2.y, vec3d2.z, 1.0D);
+        }
+    }
+
+    public BlockPos getSpawnPos() {
+        return new BlockPos(this.spawnX, this.spawnY, this.spawnZ);
+    }
+
+    public void tick() {
+        super.tick();
+        this.world.addParticle(RegisterParticles.FIREFLY, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
+        if (!this.spawnSet && !this.world.isClient) {
+            this.spawnSet=true;
+            this.spawnX=this.getBlockX();
+            this.spawnY=this.getBlockY();
+            this.spawnZ=this.getBlockZ();
+        }
+    }
+
+    protected EntityNavigation createNavigation(World world) {
+        BirdNavigation birdNavigation = new BirdNavigation(this, world) {
+            public boolean isValidPosition(BlockPos pos) {
+                return !this.world.getBlockState(pos.down()).isAir();
+            }
+        };
+        birdNavigation.setCanPathThroughDoors(false);
+        birdNavigation.setCanSwim(false);
+        birdNavigation.setCanEnterOpenDoors(true);
+        return birdNavigation;
+    }
+
+    @Override
+    public boolean isInAir() {
+        return !this.onGround;
     }
 
     @Override
     public boolean collides() {
         return false;
     }
-
-    public void tick() {
-        this.world.addParticle(RegisterParticles.FIREFLY, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
+    @Override
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+        return false;
     }
 }
