6cb4f62eeeb77cf566a30c7f5d323e908d301ee0
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-23 18:36:28 -0600

    optimized mud -> clay slightly no this didnt fix the issue

--- src/main/java/frozenblock/wild/mod/blocks/MudBlock.java
+++ src/main/java/frozenblock/wild/mod/blocks/MudBlock.java
@@ -2,25 +2,25 @@ package frozenblock.wild.mod.blocks;
 
 import frozenblock.wild.mod.registry.RegisterSounds;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.*;
 import net.minecraft.block.enums.Thickness;
-import net.minecraft.client.particle.Particle;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContext;
-import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
+import java.awt.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import static net.minecraft.block.PointedDripstoneBlock.THICKNESS;
 import static net.minecraft.block.PointedDripstoneBlock.VERTICAL_DIRECTION;
 
 public class MudBlock extends Block {
@@ -45,28 +45,77 @@ public class MudBlock extends Block {
         int y = pos.getY();
         int z = pos.getZ();
         BlockState blockState = Blocks.POINTED_DRIPSTONE.getDefaultState();
-        if (blockState.with(VERTICAL_DIRECTION, Direction.DOWN) == world.getBlockState(new BlockPos(x, y - 2, z))) {
-            world.setBlockState(new BlockPos(pos), Blocks.CLAY.getDefaultState());
-            if (!world.isClient) {
-                world.playSound(
-                        null,
-                        pos,
-                        SoundEvents.ITEM_HONEYCOMB_WAX_ON,
-                        SoundCategory.BLOCKS,
-                        1f,
-                        1f
-                );
-                ((ServerWorld) world).spawnParticles(ParticleTypes.SPLASH,
-                        x + 0.5,
-                        y + 0.5,
-                        z + 0.5,
-                        100,
-                        0.2,
-                        0.2,
-                        0.2,
-                        10
-                );
-
+        if (blockState.with(VERTICAL_DIRECTION, Direction.UP).with(THICKNESS, Thickness.TIP_MERGE) != world.getBlockState(new BlockPos(x, y - 2, z))) {
+            if (blockState.with(THICKNESS, Thickness.BASE).with(VERTICAL_DIRECTION, Direction.DOWN) == world.getBlockState(new BlockPos(x, y - 2, z))) {
+                world.setBlockState(new BlockPos(pos), Blocks.CLAY.getDefaultState());
+                if (!world.isClient) {
+                    world.playSound(
+                            null,
+                            pos,
+                            SoundEvents.ITEM_HONEYCOMB_WAX_ON,
+                            SoundCategory.BLOCKS,
+                            1f,
+                            1f
+                    );
+                    (world).spawnParticles(ParticleTypes.SPLASH,
+                            x + 0.5,
+                            y + 0.5,
+                            z + 0.5,
+                            100,
+                            0.2,
+                            0.2,
+                            0.2,
+                            10
+                    );
+                }
+            } else {
+                if (blockState.with(THICKNESS, Thickness.FRUSTUM).with(VERTICAL_DIRECTION, Direction.DOWN) == world.getBlockState(new BlockPos(x, y - 2, z))) {
+                    world.setBlockState(new BlockPos(pos), Blocks.CLAY.getDefaultState());
+                    if (!world.isClient) {
+                        world.playSound(
+                                null,
+                                pos,
+                                SoundEvents.ITEM_HONEYCOMB_WAX_ON,
+                                SoundCategory.BLOCKS,
+                                1f,
+                                1f
+                        );
+                        (world).spawnParticles(ParticleTypes.SPLASH,
+                                x + 0.5,
+                                y + 0.5,
+                                z + 0.5,
+                                100,
+                                0.2,
+                                0.2,
+                                0.2,
+                                10
+                        );
+                    }
+                } else {
+                    if (blockState.with(THICKNESS, Thickness.TIP).with(VERTICAL_DIRECTION, Direction.DOWN) == world.getBlockState(new BlockPos(x, y - 2, z))) {
+                        world.setBlockState(new BlockPos(pos), Blocks.CLAY.getDefaultState());
+                        if (!world.isClient) {
+                            world.playSound(
+                                    null,
+                                    pos,
+                                    SoundEvents.ITEM_HONEYCOMB_WAX_ON,
+                                    SoundCategory.BLOCKS,
+                                    1f,
+                                    1f
+                            );
+                            (world).spawnParticles(ParticleTypes.SPLASH,
+                                    x + 0.5,
+                                    y + 0.5,
+                                    z + 0.5,
+                                    100,
+                                    0.2,
+                                    0.2,
+                                    0.2,
+                                    10
+                            );
+                        }
+                    }
+                }
             }
         }
     }
