5152cd6b1277887b920b87c5831b11ec161e6b79
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-24 20:54:54 -0600

    added firefly spawn egg

--- src/main/java/frozenblock/wild/mod/registry/RegisterEntities.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterEntities.java
@@ -27,7 +27,7 @@ public class RegisterEntities {
     public static final EntityType<WardenEntity> WARDEN = Registry.register(Registry.ENTITY_TYPE, new Identifier(WildMod.MOD_ID, "warden"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, WardenEntity::new).dimensions(EntityDimensions.fixed(1.5f, 2.95f)).build());
     public static final EntityType<FrogEntity> FROG = Registry.register(Registry.ENTITY_TYPE, new Identifier(WildMod.MOD_ID, "frog"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FrogEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build());
     public static final EntityType<TadpoleEntity> TADPOLE = Registry.register(Registry.ENTITY_TYPE, new Identifier(WildMod.MOD_ID, "tadpole"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, TadpoleEntity::new).dimensions(EntityDimensions.fixed(0.5F, 0.3F)).build());
-    public static final EntityType<FireflyEntity> FIREFLY = Registry.register(Registry.ENTITY_TYPE, new Identifier(WildMod.MOD_ID, "firefly"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FireflyEntity::new).dimensions(EntityDimensions.fixed(0.5F, 0.3F)).build());
+    public static final EntityType<FireflyEntity> FIREFLY = Registry.register(Registry.ENTITY_TYPE, new Identifier(WildMod.MOD_ID, "firefly"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FireflyEntity::new).dimensions(EntityDimensions.fixed(0.1F, 0.1F)).build());
 
 
     public static void RegisterEntities() {
--- src/main/java/frozenblock/wild/mod/registry/RegisterItems.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterItems.java
@@ -26,6 +26,7 @@ public abstract class RegisterItems {
     public static final Item WARDEN_SPAWN_EGG = new SpawnEggItem(RegisterEntities.WARDEN, Integer.parseInt("074857", 16), Integer.parseInt("29dfeb", 16), new FabricItemSettings().group(ItemGroup.MISC));
     public static final Item FROG_SPAWN_EGG = new SpawnEggItem(RegisterEntities.FROG, Integer.parseInt("d07444", 16), Integer.parseInt("ffc77c", 16), new FabricItemSettings().group(ItemGroup.MISC));
     public static final Item TADPOLE_SPAWN_EGG = new SpawnEggItem(RegisterEntities.TADPOLE, Integer.parseInt("160a00", 16), Integer.parseInt("332115", 16), new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item FIREFLY_SPAWN_EGG = new SpawnEggItem(RegisterEntities.FIREFLY, Integer.parseInt("000000", 16), Integer.parseInt("69ff00", 16), new FabricItemSettings().group(ItemGroup.MISC));
     public static final EntityBucketItem TADPOLE_BUCKET = new EntityBucketItem(RegisterEntities.TADPOLE, Fluids.WATER, SoundEvents.ITEM_BUCKET_EMPTY_FISH, new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
 
     public static void RegisterItems() {
@@ -33,6 +34,7 @@ public abstract class RegisterItems {
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "frog_spawn_egg"), FROG_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "tadpole_bucket"), TADPOLE_BUCKET);
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "tadpole_spawn_egg"), TADPOLE_SPAWN_EGG);
+        Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "firefly_spawn_egg"), FIREFLY_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "mangrove_boat"), MANGROVE_BOAT);
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "oak_chest_boat"), OAK_CHEST_BOAT);
         Registry.register(Registry.ITEM, new Identifier(WildMod.MOD_ID, "spruce_chest_boat"), SPRUCE_CHEST_BOAT);
