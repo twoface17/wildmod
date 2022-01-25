7e398f4ef512d3134c92dd5013b7d855528d5f42
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-24 20:49:24 -0600

    reverted firefly changes from https://github.com/Osmiooo/wildmod/commit/548bbfe7e46bc6978a64e082cc28f212146694a7

--- src/main/java/frozenblock/wild/mod/entity/FireflyEntityRenderer.java
+++ src/main/java/frozenblock/wild/mod/entity/FireflyEntityRenderer.java
@@ -3,16 +3,17 @@ package frozenblock.wild.mod.entity;
 import frozenblock.wild.mod.WildMod;
 import frozenblock.wild.mod.WildModClient;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3f;
 
-public class FireflyEntityRenderer extends MobEntityRenderer<FireflyEntity, FireflyEntityModel> {
+public class FireflyEntityRenderer extends EntityRenderer<FireflyEntity> {
 
     public FireflyEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new FireflyEntityModel(context.getPart(WildModClient.MODEL_FIREFLY_LAYER)), 0.5f);
+        super(context);
     }
 
     @Override
