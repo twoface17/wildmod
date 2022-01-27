668ff8358b7cdae6e87d2d3651903da790933e24
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-27 16:22:11 -0500

    Update WardenEntitySoulsFeatureRenderer.java

--- src/main/java/frozenblock/wild/mod/entity/WardenEntitySoulsFeatureRenderer.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntitySoulsFeatureRenderer.java
@@ -3,10 +3,18 @@ package frozenblock.wild.mod.entity;
 import frozenblock.wild.mod.WildMod;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Matrix3f;
+import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec3f;
 
 @Environment(EnvType.CLIENT)
 public class WardenEntitySoulsFeatureRenderer extends EyesFeatureRenderer<WardenEntity, WardenEntityModel<WardenEntity>> {
@@ -15,6 +23,41 @@ public class WardenEntitySoulsFeatureRenderer extends EyesFeatureRenderer<Warden
 
     public WardenEntitySoulsFeatureRenderer(FeatureRendererContext<WardenEntity, WardenEntityModel<WardenEntity>> featureRendererContext) {
         super(featureRendererContext);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, WardenEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+        matrices.push();
+        MatrixStack.Entry entry = matrices.peek();
+        Matrix4f matrix4f = entry.getPositionMatrix();
+        Matrix3f matrix3f = entry.getNormalMatrix();
+
+        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(SOULS);
+
+        int j = getLight(240);
+
+        vertex(vertexConsumer, matrix4f, matrix3f, j, 0.0F, 0, 0, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, j, 1.0F, 0, 1, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, j, 1.0F, 1, 1, 0);
+        vertex(vertexConsumer, matrix4f, matrix3f, j, 0.0F, 1, 0, 0);
+
+        matrices.pop();
+        super.render(matrices, vertexConsumers, 0, entity, limbAngle, limbDistance, tickDelta, animationProgress, headYaw, headPitch);
+    }
+    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f matrix3f, int i, float f, int j, int k, int l) {
+        vertexConsumer
+                .vertex(matrix4f, f - 0.5F, j - 0.5F, 0.0F)
+                .color(0, 0, 0, 0)
+                .texture(k, l)
+                .overlay(OverlayTexture.DEFAULT_UV)
+                .light(i)
+                .normal(matrix3f, 0.0F, 1.0F, 0.0F)
+                .next();
+    }
+
+
+    protected int getLight(int i) {
+        return 240;
     }
 
     public RenderLayer getEyesTexture() {
