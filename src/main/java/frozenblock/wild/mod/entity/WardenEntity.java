f5df2f1b22fe5b9f75ab1b83e5920bb0bde592ac
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-01-30 11:04:13 -0600

    (READ DESC) made warden's vibration pitch match its anger level, as seen here: https://youtu.be/Au4gUvCUzYQ?t=80 may remove this if it doesn't sound good.

--- src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
@@ -151,7 +151,7 @@ public class WardenEntity extends HostileEntity {
             this.leaveTime = this.world.getTime() + 1200;
             this.world.sendEntityStatus(this, (byte)7);
             this.vibrationTimer=this.world.getTime();
-            this.world.playSound(null, this.getBlockPos().up(2), RegisterSounds.ENTITY_WARDEN_VIBRATION, SoundCategory.HOSTILE, 0.5F, world.random.nextFloat() * 0.2F + 0.8F);
+            this.world.playSound(null, this.getBlockPos().up(2), RegisterSounds.ENTITY_WARDEN_VIBRATION, SoundCategory.HOSTILE, 0.5F, world.random.nextFloat() * (this.trueOverallAnger()/100F) + 1.0F);
             if (vibrationPos != null) { CreateVibration(this.world, this, vibrationPos); }
             else { CreateVibration(this.world, this, lasteventpos); }
             if (eventEntity != null) {
