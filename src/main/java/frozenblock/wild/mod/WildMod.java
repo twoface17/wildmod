72207dfd92f1c1251049bdc0b1a22306c03bdb8b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-30 21:37:15 -0500

    sculkThreading Off By Default (It breaks a big radii)

--- src/main/java/frozenblock/wild/mod/WildMod.java
+++ src/main/java/frozenblock/wild/mod/WildMod.java
@@ -38,16 +38,14 @@ public class WildMod implements ModInitializer {
             GameRuleRegistry.register("catalystDetectsAll", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
     public static final GameRules.Key<GameRules.BooleanRule> SHRIEKER_GARGLES =
             GameRuleRegistry.register("shriekerGargles", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> SCULK_THREADING =
-            GameRuleRegistry.register("sculkThreading", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
     public static final GameRules.Key<GameRules.IntRule> SCULK_MULTIPLIER =
             GameRuleRegistry.register("spreadMultiplier", GameRules.Category.MISC, GameRuleFactory.createIntRule(1, 1, 20));
-    public static final GameRules.Key<GameRules.IntRule> SCULK_THREADS =
-            GameRuleRegistry.register("sculkThreads", GameRules.Category.MISC, GameRuleFactory.createIntRule(2, 1, 21));
     public static final GameRules.Key<GameRules.IntRule> UPWARD_SPREAD =
-            GameRuleRegistry.register("upwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(3, 1, 100));
+            GameRuleRegistry.register("upwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(12, 1, 100));
     public static final GameRules.Key<GameRules.IntRule> DOWNWARD_SPREAD =
-            GameRuleRegistry.register("downwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(8, 1, 100));
+            GameRuleRegistry.register("downwardSpread", GameRules.Category.MISC, GameRuleFactory.createIntRule(12, 1, 100));
+    public static final GameRules.Key<GameRules.BooleanRule> SCULK_THREADING =
+            GameRuleRegistry.register("sculkThreading", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
     //TESTING GAMERULES. TODO: DELETE THESE AT 1.0. DO NOT WRITE TOO MUCH CODE THAT DEPENDS ON THESE.
     public static final GameRules.Key<GameRules.BooleanRule> NO_WARDEN_COOLDOWN =
