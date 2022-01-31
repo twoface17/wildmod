e36edaedbcf79a48f35d390ce68e75821c3b5052
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-31 13:50:53 -0500

    Anim Testing 2

--- src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntity.java
@@ -81,7 +81,7 @@ public class WardenEntity extends HostileEntity {
             if (this.attackTicksLeft1 > 0) { --this.attackTicksLeft1; }
             if (this.roarTicksLeft1 > 0) { --this.roarTicksLeft1; }
             if (this.attackCooldown > 0) { --this.attackCooldown; }
-            if (this.sniffTicksLeft>43) {this.world.sendEntityStatus(this, (byte)23);}
+            if (this.sniffTicksLeft>=44) {this.world.sendEntityStatus(this, (byte)23);}
             this.tickEmerge();
             this.tickStuck();
             this.tickSniff();
@@ -597,4 +597,7 @@ public class WardenEntity extends HostileEntity {
     //ANIMATION
     public float emergeAnimStartTime;
     public float emergeAnimTime;
+
+    public float sniffAnimStartTime;
+    public float sniffAnimTime;
 }
