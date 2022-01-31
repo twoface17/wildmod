7a711dc94774bda4492ec3dacb181efb2878f2df
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-31 17:56:14 -0500

    Update NewProperties.java

--- src/main/java/frozenblock/wild/mod/fromAccurateSculk/NewProperties.java
+++ src/main/java/frozenblock/wild/mod/fromAccurateSculk/NewProperties.java
@@ -1,5 +1,6 @@
 package frozenblock.wild.mod.fromAccurateSculk;
 
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 
@@ -7,5 +8,6 @@ public class NewProperties extends Properties {
 
     public static final EnumProperty<SculkCatalystPhase> SCULK_CATALYST_PHASE = EnumProperty.of("sculk_catalyst_phase", SculkCatalystPhase.class);
     public static final EnumProperty<SculkShriekerPhase> SCULK_SHRIEKER_PHASE = EnumProperty.of("sculk_shrieker_phase", SculkShriekerPhase.class);
+    public static final BooleanProperty ORIGINAL_RF = BooleanProperty.of("original_rf");
 
 }
