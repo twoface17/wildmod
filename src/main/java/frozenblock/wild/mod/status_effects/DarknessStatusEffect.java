c7ba81dc7a7a03232f2e4ba17764510a2289c1e6
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-02-06 15:27:50 -0600

    test

--- src/main/java/frozenblock/wild/mod/status_effects/DarknessStatusEffect.java
+++ src/main/java/frozenblock/wild/mod/status_effects/DarknessStatusEffect.java
@@ -2,6 +2,8 @@ package frozenblock.wild.mod.status_effects;
 
 import frozenblock.wild.mod.liukrastapi.MathAddon;
 import frozenblock.wild.mod.registry.RegisterSounds;
+import frozenblock.wild.mod.registry.RegisterStatusEffects;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
@@ -16,10 +18,20 @@ public class DarknessStatusEffect extends StatusEffect {
                 0x000000
         );
     }
+    public double time;
+
+    public void tick(MinecraftClient client) {
+        if(client.player.hasStatusEffect(RegisterStatusEffects.DARKNESS)) {
+            time = time + 0.075/2;
+            MathAddon.time = time;
+        } else {
+            time = 0;
+        }
+    }
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         float pulse;
-        pulse = (float) MathAddon.cutCos(MathAddon.time, 0, false);
-        if(pulse < 0.5f && pulse > 0.4f) {
+        pulse = (float) MathAddon.cutCos(time, 0, false) * 1.5f;
+        if(pulse < 1.51f && pulse > 1.45f) {
             World world = entity.getEntityWorld();
             BlockPos pos = entity.getBlockPos();
             if(!world.isClient) {
@@ -28,7 +40,7 @@ public class DarknessStatusEffect extends StatusEffect {
                         pos,
                         RegisterSounds.ENTITY_WARDEN_CLOSE,
                         SoundCategory.AMBIENT,
-                        5f,
+                        1f,
                         1f
                 );
             }
