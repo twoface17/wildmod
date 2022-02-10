e27b2e24a9a51d289f83c24b8eae743655689f4f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-02-09 19:46:58 -0500

    Can't Cheese With Water Bucket Again

---
+++ src/main/java/frozenblock/wild/mod/liukrastapi/WardenSwimGoal.java
@@ -0,0 +1,37 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package frozenblock.wild.mod.liukrastapi;
+
+import java.util.EnumSet;
+
+import frozenblock.wild.mod.entity.WardenEntity;
+import net.minecraft.entity.ai.goal.Goal;
+
+public class WardenSwimGoal extends Goal {
+    private final WardenEntity mob;
+
+    public WardenSwimGoal(WardenEntity mob) {
+        this.mob = mob;
+        this.setControls(EnumSet.of(Control.JUMP));
+        mob.getNavigation().setCanSwim(true);
+    }
+
+    public boolean canStart() {
+        if (this.mob.emergeTicksLeft>0 || this.mob.emergeTicksLeft==-5) {return false;}
+        return this.mob.isTouchingWater() && this.mob.isSubmergedInWater() || this.mob.isInLava();
+    }
+
+    public boolean shouldRunEveryTick() {
+        return true;
+    }
+
+    public void tick() {
+        if (this.mob.getRandom().nextFloat() < 0.8F) {
+            this.mob.getJumpControl().setActive();
+        }
+
+    }
+}
