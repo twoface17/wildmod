7168d65a77edb478b440fbdf03b3f10f4bcff865
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-02-11 17:50:37 -0500

    Fix Occasional Tendril Z-Fighting
    Most noticeable when the Warden's head is at a relatively strange angle, which was more noticeable with the new head rolling. Haven't seen it since this change.

--- src/main/java/frozenblock/wild/mod/entity/WardenEntityModel.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntityModel.java
@@ -35,8 +35,8 @@ public class WardenEntityModel<T extends WardenEntity> extends EntityModel<Warde
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData modelPartData1 = modelPartData.addChild("body", ModelPartBuilder.create().uv(0,26).cuboid(-9.0F, -21.0F, -5.0F, 18.0F, 21.0F, 11.0F), ModelTransform.pivot(0.0F,11.0F,0.0F));
         ModelPartData modelPartData2 = modelPartData1.addChild("head", ModelPartBuilder.create().uv(0,0).cuboid(-8.0F, -16.0F, -5.0F, 16.0F, 16.0F, 10.0F), ModelTransform.pivot(0.0F,-21.0F,0.0F));
-        modelPartData2.addChild("right_tendril", ModelPartBuilder.create().uv(106,36).cuboid(-10.0F, -6.5F, 0.0F, 10.0F, 10.0F, 0.001F), ModelTransform.pivot(-8.0F,-12.5F,0.0F));
-        modelPartData2.addChild("left_tendril", ModelPartBuilder.create().uv(106,46).cuboid(0.0F, -6.5F, 0.0F, 10.0F, 10.0F, 0.001F), ModelTransform.pivot(8.0F,-12.5F,0.0F));
+        modelPartData2.addChild("right_tendril", ModelPartBuilder.create().uv(106,36).cuboid(-10.0F, -6.5F, 0.0F, 10.0F, 10.0F, 0.002F), ModelTransform.pivot(-8.0F,-12.5F,0.0F));
+        modelPartData2.addChild("left_tendril", ModelPartBuilder.create().uv(106,46).cuboid(0.0F, -6.5F, 0.0F, 10.0F, 10.0F, 0.002F), ModelTransform.pivot(8.0F,-12.5F,0.0F));
         modelPartData1.addChild("right_arm", ModelPartBuilder.create().uv(52,0).cuboid(-6.0F, -4.0F, -4.0F, 8.0F, 28.0F, 8.0F), ModelTransform.pivot(-11.0F,-17.0F,0.0F));
         modelPartData1.addChild("left_arm", ModelPartBuilder.create().uv(84,0).cuboid(-2.0F, -4.0F, -4.0F, 8.0F, 28.0F, 8.0F), ModelTransform.pivot(11.0F,-17.0F,0.0F));
         modelPartData.addChild("left_leg", ModelPartBuilder.create().uv(82,36).cuboid(-3.0F, 0.0F, -3.0F, 6.0F, 13.0F, 6.0F), ModelTransform.pivot(6.0F,11.0F,0.0F));
