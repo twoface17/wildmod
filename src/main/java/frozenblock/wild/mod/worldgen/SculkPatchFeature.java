2d8c0a33d39a81df2b5a97c6bde71d830d29389b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-02-14 16:34:56 -0500

    Sculk Patch Generation W/Activators
    Generates Sensors, Shriekers, Sculk, and Veins, mimicking the Catalyst's spreading through SimpleRandom noise.

--- src/main/java/frozenblock/wild/mod/worldgen/SculkPatchFeature.java
+++ src/main/java/frozenblock/wild/mod/worldgen/SculkPatchFeature.java
@@ -1,15 +1,27 @@
 package frozenblock.wild.mod.worldgen;
 
 import com.mojang.serialization.Codec;
+import frozenblock.wild.mod.blocks.SculkShriekerBlock;
+import frozenblock.wild.mod.blocks.SculkVeinBlock;
+import frozenblock.wild.mod.fromAccurateSculk.SculkTags;
 import frozenblock.wild.mod.registry.RegisterBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.noise.PerlinNoiseSampler;
+import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
+import net.minecraft.world.gen.random.SimpleRandom;
 
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.function.Predicate;
 
 public class SculkPatchFeature extends Feature<DefaultFeatureConfig> {
     Random random = new Random();
@@ -40,13 +52,22 @@ public class SculkPatchFeature extends Feature<DefaultFeatureConfig> {
      And with that in mind, happy coding!
      */
 
+    //alex im sorry that i made your code a mess but it's the only way i know how to do this
+
     @Override
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
         boolean success = true;
+        if (seed!=context.getWorld().getSeed()) {
+            seed=context.getWorld().getSeed();
+            sample = new PerlinNoiseSampler(new SimpleRandom(seed));
+        }
         //Places the catalyst
-        context.getWorld().setBlockState(context.getOrigin(), RegisterBlocks.SCULK_CATALYST.getDefaultState(), 0);
-
+        context.getWorld().setBlockState(context.getOrigin().up(), RegisterBlocks.SCULK_CATALYST.getDefaultState(), 0);
+        placePatch(context, context.getOrigin(), random.nextInt(8,24));
         //This code randomly places a few sculk patches around. it repeats a random place selection forever, with a 40% chance of stopping every time and if it reaches 6, it automatically stops it after the 6th patch.
+
+        //NOTE FROM LUNADE/A VIEW FROM THE TOP: I don't think the stuff below here works (and idk why,) but using noise and a random radius should eliminate the need for it anyway.
+        //TL;DR - I don't think anything below here is needed
 
         //TABLE OF PROBABILITY
         //1 Patch: 100.00%
@@ -79,9 +100,9 @@ public class SculkPatchFeature extends Feature<DefaultFeatureConfig> {
                 }
             }
             //Checks if the block at the specified location is air and within 6 blocks of the origin. This only affects the center of the patches.
-            if (context.getOrigin().ORIGIN.isWithinDistance(patchPos, 6) && context.getWorld().testBlockState(patchPos, Predicate.isEqual(Blocks.AIR.getDefaultState()))) {
+            if (context.getOrigin().ORIGIN.isWithinDistance(patchPos, 6)) {
                 //Places a patch and gives a chance of stopping.
-                placePatch(context, patchPos);
+                placePatch(context, patchPos, random.nextInt(64)); //This won't run for some reason
                 if (Math.random() < 0.40) {
                     break;
                 }
@@ -95,9 +116,158 @@ public class SculkPatchFeature extends Feature<DefaultFeatureConfig> {
 
         return success;
     }
+    /** NOISE VARIABLES */
+    public static final double multiplier = 0.20; //Lowering this makes the noise bigger, raising it makes it smaller (more like static)
+    public static final double minThreshold = -0.3; //The value that outer Sculk's noise must be ABOVE in order to grow
+    public static final double maxThreshold = 0.3; //The value that outer Sculk's noise must be BELOW in order to grow
+    public static long seed = 1;
+    public static PerlinNoiseSampler sample = new PerlinNoiseSampler(new SimpleRandom(seed));
 
-    public void placePatch(FeatureContext<DefaultFeatureConfig> context, BlockPos pos) {
-        //Here's where you put the code for placing a patch. For default, ive made it place a sculk block. This will need to change.
-        context.getWorld().setBlockState(pos, RegisterBlocks.SCULK.getDefaultState(), 0);
+    public void placePatch(FeatureContext<DefaultFeatureConfig> context, BlockPos pos, int r) {
+        StructureWorldAccess world = context.getWorld();
+        //Place Sculk
+        for (BlockPos blockpos : blockTagsInSphere(pos, r, SculkTags.BLOCK_REPLACEABLE, world)) {
+            double sampled = sample.sample(blockpos.getX()*multiplier, blockpos.getY()*multiplier,blockpos.getZ()*multiplier);
+            if (sampled>minThreshold && sampled<maxThreshold && blockpos.getY()<maxSculk) {
+                world.setBlockState(blockpos, RegisterBlocks.SCULK.getDefaultState(), 0);
+            }
+        }
+        //Place Veins
+        for (BlockPos blockpos : solidInSphere(pos, r+2, world)) {
+            double sampled = sample.sample(blockpos.getX()*multiplier, blockpos.getY()*multiplier,blockpos.getZ()*multiplier);
+            if (sampled<minThreshold && sampled>minThreshold-0.16 && blockpos.getY()<maxSculk+2) {
+                for (Direction direction : Direction.values()) {
+                    BlockPos pos1 = blockpos.offset(direction);
+                    BlockState state = world.getBlockState(pos1);
+                    Block block = state.getBlock();
+                    if (SculkTags.ALWAYS_WATER.contains(block) || state == Blocks.WATER.getDefaultState()) {
+                        world.setBlockState(pos1, RegisterBlocks.SCULK_VEIN.getDefaultState().with(waterLogged, true).with(getOpposite(direction), true),0);
+                    } else if (block != waterBlock) {
+                        if (block == veinBlock) {
+                            world.setBlockState(pos1, state.with(getOpposite(direction), true),0);
+                        } else if (SculkTags.SCULK_VEIN_REPLACEABLE.contains(block) || state.isAir()) {
+                            world.setBlockState(pos1, RegisterBlocks.SCULK_VEIN.getDefaultState().with(getOpposite(direction), true),0);
+                        }
+                    }
+                }
+            }
+        }
+        for (BlockPos blockpos : solidInSphere(pos, r+2, world)) {
+            double sampled = sample.sample(blockpos.getX()*multiplier, blockpos.getY()*multiplier,blockpos.getZ()*multiplier);
+            if (sampled>maxThreshold && sampled<maxThreshold+0.16 && blockpos.getY()<maxSculk+2) {
+                for (Direction direction : Direction.values()) {
+                    BlockPos pos1 = blockpos.offset(direction);
+                    BlockState state = world.getBlockState(pos1);
+                    Block block = state.getBlock();
+                    if (SculkTags.ALWAYS_WATER.contains(block) || state == Blocks.WATER.getDefaultState()) {
+                        world.setBlockState(pos1, RegisterBlocks.SCULK_VEIN.getDefaultState().with(waterLogged, true).with(getOpposite(direction), true),0);
+                    } else if (block != waterBlock) {
+                        if (block == veinBlock) {
+                            world.setBlockState(pos1, state.with(getOpposite(direction), true),0);
+                        } else if (SculkTags.SCULK_VEIN_REPLACEABLE.contains(block) || state.isAir()) {
+                            world.setBlockState(pos1, RegisterBlocks.SCULK_VEIN.getDefaultState().with(getOpposite(direction), true),0);
+                        }
+                    }
+                }
+            }
+        }
+        //Place Activators
+        for (BlockPos blockpos : blocksInSphere(pos, r, RegisterBlocks.SCULK, world)) {
+            double sampled = sample.sample(blockpos.getX(), blockpos.getY(),blockpos.getZ());
+            if (SculkTags.SCULK_VEIN_REPLACEABLE.contains(world.getBlockState(blockpos.up()).getBlock())) {
+                //SENSOR
+                if (sampled<0.55 && sampled>0.38 && blockpos.getY()<maxSculk) {
+                    if ((world.getBlockState(blockpos.up()).contains(waterLogged) && world.getBlockState(blockpos.up()).get(waterLogged)) || world.getBlockState(blockpos.up()) == water) {
+                        world.setBlockState(blockpos.up(), Blocks.SCULK_SENSOR.getDefaultState().with(waterLogged, true), 0);
+                    } else {
+                        world.setBlockState(blockpos.up(), Blocks.SCULK_SENSOR.getDefaultState(), 0);
+                    }
+                }
+                //SHRIEKER
+                if (sampled<1 && sampled>0.63 && blockpos.getY()<maxSculk) {
+                    if ((world.getBlockState(blockpos.up()).contains(waterLogged) && world.getBlockState(blockpos.up()).get(waterLogged)) || world.getBlockState(blockpos.up())==water) {
+                        world.setBlockState(blockpos.up(), SculkShriekerBlock.SCULK_SHRIEKER_BLOCK.getDefaultState().with(waterLogged, true), 0);
+                } else {
+                        world.setBlockState(blockpos.up(), SculkShriekerBlock.SCULK_SHRIEKER_BLOCK.getDefaultState(), 0); }
+                }
+            }
+        }
+    }
+
+    public static BooleanProperty getOpposite(Direction direction) {
+        if (direction==Direction.UP) { return Properties.DOWN; }
+        if (direction==Direction.DOWN) { return Properties.UP; }
+        if (direction==Direction.NORTH) { return Properties.SOUTH; }
+        if (direction==Direction.SOUTH) { return Properties.NORTH; }
+        if (direction==Direction.EAST) { return Properties.WEST; }
+        if (direction==Direction.WEST) { return Properties.EAST; }
+        return Properties.DOWN;
+    }
+    public static final BlockState water = Blocks.WATER.getDefaultState();
+    public static final Block waterBlock = Blocks.WATER;
+    public static final Block veinBlock = SculkVeinBlock.SCULK_VEIN;
+    public static final BooleanProperty waterLogged = Properties.WATERLOGGED;
+    public static final int maxSculk = 56;
+
+    public static ArrayList<BlockPos> blockTagsInSphere(BlockPos pos, int radius, Tag<Block> tag, StructureWorldAccess world) {
+        int bx = pos.getX();
+        int by = pos.getY();
+        int bz = pos.getZ();
+        ArrayList<BlockPos> blocks = new ArrayList<>();
+        for(int x = bx - radius; x <= bx + radius; x++) {
+            for(int y = by - radius; y <= by + radius; y++) {
+                for(int z = bz - radius; z <= bz + radius; z++) {
+                    double distance = ((bx-x) * (bx-x) + ((bz-z) * (bz-z)) + ((by-y) * (by-y)));
+                    if(distance < radius * radius) {
+                        BlockPos l = new BlockPos(x, y, z);
+                        if (tag.contains(world.getBlockState(l).getBlock())) {
+                            blocks.add(l);
+                        }
+                    }
+                }
+            }
+        }
+        return blocks;
+    }
+
+    public static ArrayList<BlockPos> solidInSphere(BlockPos pos, int radius, StructureWorldAccess world) {
+        int bx = pos.getX();
+        int by = pos.getY();
+        int bz = pos.getZ();
+        ArrayList<BlockPos> blocks = new ArrayList<>();
+        for(int x = bx - radius; x <= bx + radius; x++) {
+            for(int y = by - radius; y <= by + radius; y++) {
+                for(int z = bz - radius; z <= bz + radius; z++) {
+                    double distance = ((bx-x) * (bx-x) + ((bz-z) * (bz-z)) + ((by-y) * (by-y)));
+                    if(distance < radius * radius) {
+                        BlockPos l = new BlockPos(x, y, z);
+                        if (world.getBlockState(l).isFullCube(world, l) && !SculkTags.SCULK.contains(world.getBlockState(l).getBlock())) {
+                            blocks.add(l);
+                        }
+                    }
+                }
+            }
+        }
+        return blocks;
+    }
+    public static ArrayList<BlockPos> blocksInSphere(BlockPos pos, int radius, Block block, StructureWorldAccess world) {
+        ArrayList<BlockPos> blocks = new ArrayList<>();
+        int bx = pos.getX();
+        int by = pos.getY();
+        int bz = pos.getZ();
+        for(int x = bx - radius; x <= bx + radius; x++) {
+            for(int y = by - radius; y <= by + radius; y++) {
+                for(int z = bz - radius; z <= bz + radius; z++) {
+                    double distance = ((bx-x) * (bx-x) + ((bz-z) * (bz-z)) + ((by-y) * (by-y)));
+                    if(distance < radius * radius) {
+                        BlockPos l = new BlockPos(x, y, z);
+                        if (world.getBlockState(l).getBlock() == block) {
+                            blocks.add(l);
+                        }
+                    }
+                }
+            }
+        }
+        return blocks;
     }
 }
