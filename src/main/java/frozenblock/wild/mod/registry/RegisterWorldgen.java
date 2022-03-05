eb3662b8cc5a6715112db1b24b0a84dfb1d65e4d
Treetrain1 <treetrain1@gmail.com>
2022-03-05 17:20:19 -0600

    Update Fabric API and remove decorator import

--- src/main/java/frozenblock/wild/mod/registry/RegisterWorldgen.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterWorldgen.java
@@ -26,7 +26,6 @@ import net.minecraft.world.biome.SpawnSettings;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.blockpredicate.BlockPredicate;
-import net.minecraft.world.gen.decorator.*;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
