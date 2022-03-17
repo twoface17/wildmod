e6fd8b0d8f24575abae285d7681bb9d2bead5a5b
Xfrtrex <XfrPLayz@gmail.com>
2022-03-17 10:19:24 -0700

    mud collision shape

--- src/main/java/frozenblock/wild/mod/registry/RegisterBlocks.java
+++ src/main/java/frozenblock/wild/mod/registry/RegisterBlocks.java
@@ -2,7 +2,7 @@ package frozenblock.wild.mod.registry;
 
 import frozenblock.wild.mod.WildMod;
 import frozenblock.wild.mod.blocks.*;
-import frozenblock.wild.mod.blocks.RootedMudBlock;
+import frozenblock.wild.mod.blocks.mangrove.MuddyMangroveRoots;
 import frozenblock.wild.mod.items.FrogSpawnItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -66,7 +66,7 @@ public abstract class RegisterBlocks {
     public static final Block REINFORCED_DEEPSLATE = new PillarBlock(REINFORCED_DEEPSLATE_SETTINGS);
 
     public static final Block MUD_BLOCK = new MudBlock();
-    public static final Block MUDDY_MANGROVE_ROOTS = new RootedMudBlock();
+    public static final Block MUDDY_MANGROVE_ROOTS = new MuddyMangroveRoots();
 
     public static final Block MUD_BRICKS = new MudBricks(MUD_BRICKS_SETTINGS);
     public static final WallBlock MUD_BRICKS_WALL = new MudBricksWall(MUD_BRICKS_SETTINGS);
