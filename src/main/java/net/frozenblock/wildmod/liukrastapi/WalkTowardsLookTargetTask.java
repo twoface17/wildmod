3bab48d0005c360aec4ef0691e2144e791eff144
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-04-30 11:39:02 -0700

    Echo Shards And Unfunctional Recovery Compass

--- src/main/java/net/frozenblock/wildmod/liukrastapi/WalkTowardsLookTargetTask.java
+++ src/main/java/net/frozenblock/wildmod/liukrastapi/WalkTowardsLookTargetTask.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.LookTargetUtil;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.Optional;
@@ -37,6 +38,7 @@ public class WalkTowardsLookTargetTask<E extends LivingEntity> extends Task<E> {
         }
     }
 
+    @Nullable
     protected void run(ServerWorld world, E entity, long time) {
         //LookTargetUtil.walkTowards((LivingEntity) entity, (Entity) ((Optional)this.lookTargetFunction.apply(entity)).get(), this.speed, this.completionRange);
     }
