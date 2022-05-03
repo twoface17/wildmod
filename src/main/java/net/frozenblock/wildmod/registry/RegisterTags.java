c857843d4a2f2b41c6e3408dbd549469ca0705e1
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2022-05-03 11:14:49 -0500

    pls work

--- src/main/java/net/frozenblock/wildmod/registry/RegisterTags.java
+++ src/main/java/net/frozenblock/wildmod/registry/RegisterTags.java
@@ -1,7 +1,7 @@
 package net.frozenblock.wildmod.registry;
 
 import net.frozenblock.wildmod.WildMod;
-import net.frozenblock.wildmod.entity.FrogVariant;
+import net.frozenblock.wildmod.entity.FrogEntity;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityType;
 import net.minecraft.tag.TagKey;
@@ -13,8 +13,8 @@ public class RegisterTags {
     public static final TagKey<Block> FROG_PREFER_JUMP_TO = TagKey.of(Registry.BLOCK_KEY, new Identifier(WildMod.MOD_ID, "frog_prefer_jump_to"));
     public static final TagKey<Block> MANGROVE_LOGS_CAN_GROW_THROUGH = TagKey.of(Registry.BLOCK_KEY, new Identifier(WildMod.MOD_ID, "mangrove_logs_can_grow_through"));
     public static final TagKey<Block> MANGROVE_ROOTS_CAN_GROW_THROUGH = TagKey.of(Registry.BLOCK_KEY, new Identifier(WildMod.MOD_ID, "mangrove_roots_can_grow_through"));
-    public static final TagKey<FrogVariant> TEMPERATE = TagKey.of(FrogVariant.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/temperate_frog.png"));
-    public static final TagKey<FrogVariant> WARM = TagKey.of(FrogVariant.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/warm_frog.png"));
-    public static final TagKey<FrogVariant> COLD = TagKey.of(FrogVariant.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/cold_frog.png"));
+    public static final TagKey<FrogEntity.Variant> TEMPERATE = TagKey.of(net.frozenblock.wildmod.registry.Registry.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/temperate_frog.png"));
+    public static final TagKey<FrogEntity.Variant> WARM = TagKey.of(net.frozenblock.wildmod.registry.Registry.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/warm_frog.png"));
+    public static final TagKey<FrogEntity.Variant> COLD = TagKey.of(net.frozenblock.wildmod.registry.Registry.FROG_VARIANT_KEY, new Identifier(WildMod.MOD_ID, "textures/entity/frog/cold_frog.png"));
     public static final TagKey<EntityType<?>> FROG_FOOD = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(WildMod.MOD_ID, "frog_food"));
 }
