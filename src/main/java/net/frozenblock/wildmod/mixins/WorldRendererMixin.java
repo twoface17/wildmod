adb50ec0550efd303b84aa875b2816f7dbc18426
Treetrain1 <treetrain1@gmail.com>
2022-05-15 16:54:24 -0500

    darkness aa pls help

--- src/main/java/net/frozenblock/wildmod/mixins/WorldRendererMixin.java
+++ src/main/java/net/frozenblock/wildmod/mixins/WorldRendererMixin.java
@@ -1,28 +1,37 @@
 package net.frozenblock.wildmod.mixins;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.frozenblock.wildmod.block.MultifaceGrowthBlock;
 import net.frozenblock.wildmod.block.SculkShriekerBlock;
 import net.frozenblock.wildmod.particle.SculkChargeParticleEffect;
 import net.frozenblock.wildmod.particle.ShriekParticleEffect;
 import net.frozenblock.wildmod.registry.RegisterParticles;
 import net.frozenblock.wildmod.registry.RegisterSounds;
-import net.minecraft.client.render.WorldRenderer;
+import net.frozenblock.wildmod.registry.RegisterStatusEffects;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.VertexBuffer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.ParticleUtil;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.*;
 import net.minecraft.util.math.intprovider.IntProvider;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -33,9 +42,164 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 @Mixin(WorldRenderer.class)
-public class WorldRendererMixin {
+public abstract class WorldRendererMixin {
 
     @Shadow @Nullable private ClientWorld world;
+
+    @Shadow @Final private MinecraftClient client;
+
+    @Shadow protected abstract void renderEndSky(MatrixStack matrices);
+
+    @Shadow @Nullable private VertexBuffer lightSkyBuffer;
+
+    @Shadow @Final private static Identifier SUN;
+
+    @Shadow @Final private static Identifier MOON_PHASES;
+
+    @Shadow @Nullable private VertexBuffer starsBuffer;
+
+    @Shadow @Nullable private VertexBuffer darkSkyBuffer;
+
+    private boolean method_43788(Camera camera) {
+        Entity var3 = camera.getFocusedEntity();
+        if (!(var3 instanceof LivingEntity livingEntity)) {
+            return false;
+        } else {
+            return livingEntity.hasStatusEffect(StatusEffects.BLINDNESS) || livingEntity.hasStatusEffect(RegisterStatusEffects.DARKNESS);
+        }
+    }
+
+    @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"))
+    public void renderSky(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
+        runnable.run();
+        if (!bl) {
+            CameraSubmersionType cameraSubmersionType = camera.getSubmersionType();
+            if (cameraSubmersionType != CameraSubmersionType.POWDER_SNOW && cameraSubmersionType != CameraSubmersionType.LAVA && !this.method_43788(camera)) {
+                if (this.client.world.getDimensionEffects().getSkyType() == DimensionEffects.SkyType.END) {
+                    this.renderEndSky(matrices);
+                } else if (this.client.world.getDimensionEffects().getSkyType() == DimensionEffects.SkyType.NORMAL) {
+                    RenderSystem.disableTexture();
+                    Vec3d vec3d = this.world.getSkyColor(this.client.gameRenderer.getCamera().getPos(), tickDelta);
+                    float f = (float)vec3d.x;
+                    float g = (float)vec3d.y;
+                    float h = (float)vec3d.z;
+                    BackgroundRenderer.setFogBlack();
+                    BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+                    RenderSystem.depthMask(false);
+                    RenderSystem.setShaderColor(f, g, h, 1.0F);
+                    Shader shader = RenderSystem.getShader();
+                    this.lightSkyBuffer.bind();
+                    this.lightSkyBuffer.setShader(matrices.peek().getPositionMatrix(), projectionMatrix, shader);
+                    VertexBuffer.unbind();
+                    RenderSystem.enableBlend();
+                    RenderSystem.defaultBlendFunc();
+                    float[] fs = this.world.getDimensionEffects().getFogColorOverride(this.world.getSkyAngle(tickDelta), tickDelta);
+                    float i;
+                    float k;
+                    float o;
+                    float p;
+                    float q;
+                    if (fs != null) {
+                        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+                        RenderSystem.disableTexture();
+                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                        matrices.push();
+                        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(90.0F));
+                        i = MathHelper.sin(this.world.getSkyAngleRadians(tickDelta)) < 0.0F ? 180.0F : 0.0F;
+                        matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(i));
+                        matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(90.0F));
+                        float j = fs[0];
+                        k = fs[1];
+                        float l = fs[2];
+                        Matrix4f matrix4f = matrices.peek().getPositionMatrix();
+                        bufferBuilder.begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
+                        bufferBuilder.vertex(matrix4f, 0.0F, 100.0F, 0.0F).color(j, k, l, fs[3]).next();
+                        int m = 16;
+
+                        for(int n = 0; n <= 16; ++n) {
+                            o = (float)n * 6.2831855F / 16.0F;
+                            p = MathHelper.sin(o);
+                            q = MathHelper.cos(o);
+                            bufferBuilder.vertex(matrix4f, p * 120.0F, q * 120.0F, -q * 40.0F * fs[3]).color(fs[0], fs[1], fs[2], 0.0F).next();
+                        }
+
+                        bufferBuilder.end();
+                        BufferRenderer.draw(bufferBuilder);
+                        matrices.pop();
+                    }
+
+                    RenderSystem.enableTexture();
+                    RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
+                    matrices.push();
+                    i = 1.0F - this.world.getRainGradient(tickDelta);
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, i);
+                    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-90.0F));
+                    matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(this.world.getSkyAngle(tickDelta) * 360.0F));
+                    Matrix4f matrix4f2 = matrices.peek().getPositionMatrix();
+                    k = 30.0F;
+                    RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                    RenderSystem.setShaderTexture(0, SUN);
+                    bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+                    bufferBuilder.vertex(matrix4f2, -k, 100.0F, -k).texture(0.0F, 0.0F).next();
+                    bufferBuilder.vertex(matrix4f2, k, 100.0F, -k).texture(1.0F, 0.0F).next();
+                    bufferBuilder.vertex(matrix4f2, k, 100.0F, k).texture(1.0F, 1.0F).next();
+                    bufferBuilder.vertex(matrix4f2, -k, 100.0F, k).texture(0.0F, 1.0F).next();
+                    bufferBuilder.end();
+                    BufferRenderer.draw(bufferBuilder);
+                    k = 20.0F;
+                    RenderSystem.setShaderTexture(0, MOON_PHASES);
+                    int r = this.world.getMoonPhase();
+                    int s = r % 4;
+                    int m = r / 4 % 2;
+                    float t = (float)(s + 0) / 4.0F;
+                    o = (float)(m + 0) / 2.0F;
+                    p = (float)(s + 1) / 4.0F;
+                    q = (float)(m + 1) / 2.0F;
+                    bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+                    bufferBuilder.vertex(matrix4f2, -k, -100.0F, k).texture(p, q).next();
+                    bufferBuilder.vertex(matrix4f2, k, -100.0F, k).texture(t, q).next();
+                    bufferBuilder.vertex(matrix4f2, k, -100.0F, -k).texture(t, o).next();
+                    bufferBuilder.vertex(matrix4f2, -k, -100.0F, -k).texture(p, o).next();
+                    bufferBuilder.end();
+                    BufferRenderer.draw(bufferBuilder);
+                    RenderSystem.disableTexture();
+                    float u = this.world.method_23787(tickDelta) * i;
+                    if (u > 0.0F) {
+                        RenderSystem.setShaderColor(u, u, u, u);
+                        BackgroundRenderer.clearFog();
+                        this.starsBuffer.bind();
+                        this.starsBuffer.setShader(matrices.peek().getPositionMatrix(), projectionMatrix, GameRenderer.getPositionShader());
+                        VertexBuffer.unbind();
+                        runnable.run();
+                    }
+
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                    RenderSystem.disableBlend();
+                    matrices.pop();
+                    RenderSystem.disableTexture();
+                    RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, 1.0F);
+                    double d = this.client.player.getCameraPosVec(tickDelta).y - this.world.getLevelProperties().getSkyDarknessHeight(this.world);
+                    if (d < 0.0) {
+                        matrices.push();
+                        matrices.translate(0.0, 12.0, 0.0);
+                        this.darkSkyBuffer.bind();
+                        this.darkSkyBuffer.setShader(matrices.peek().getPositionMatrix(), projectionMatrix, shader);
+                        VertexBuffer.unbind();
+                        matrices.pop();
+                    }
+
+                    if (this.world.getDimensionEffects().isAlternateSkyColor()) {
+                        RenderSystem.setShaderColor(f * 0.2F + 0.04F, g * 0.2F + 0.04F, h * 0.6F + 0.1F, 1.0F);
+                    } else {
+                        RenderSystem.setShaderColor(f, g, h, 1.0F);
+                    }
+
+                    RenderSystem.enableTexture();
+                    RenderSystem.depthMask(true);
+                }
+            }
+        }
+    }
 
     @Inject(method = "processWorldEvent", at = @At("TAIL"))
     public void processWorldEvent(PlayerEntity source, int eventId, BlockPos pos, int data, CallbackInfo ci) {
