2139bc0bf5ab6a1e1eb653761371a475b6cc36d9
Treetrain1 <treetrain1@gmail.com>
2022-05-17 21:27:44 -0500

    i wonder if this will fix vibrations idk

---
+++ src/main/java/net/frozenblock/wildmod/mixins/SculkSensorBlockEntityMixin.java
@@ -0,0 +1,50 @@
+package net.frozenblock.wildmod.mixins;
+
+import net.frozenblock.wildmod.event.GameEvent;
+import net.frozenblock.wildmod.event.GameEventListener;
+import net.frozenblock.wildmod.event.VibrationListener;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SculkSensorBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.SculkSensorBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(SculkSensorBlockEntity.class)
+public class SculkSensorBlockEntityMixin extends BlockEntity implements VibrationListener.Callback {
+    public SculkSensorBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Shadow public static int getPower(int distance, int range) {
+        double d = (double)distance / (double)range;
+        return Math.max(1, 15 - MathHelper.floor(d * 15.0));
+    }
+
+    @Shadow private int lastVibrationFrequency;
+
+    @Override
+    public boolean accepts(ServerWorld world,GameEventListener listener, BlockPos pos, GameEvent event, @Nullable GameEvent.Emitter emitter) {
+        return !this.isRemoved() && (!pos.equals(this.getPos()) || event != net.minecraft.world.event.GameEvent.BLOCK_DESTROY && event != net.minecraft.world.event.GameEvent.BLOCK_PLACE)
+                ? SculkSensorBlock.isInactive(this.getCachedState())
+                : false;
+    }
+
+    @Override
+    public void accept(
+            ServerWorld world, GameEventListener listener, BlockPos pos, GameEvent event, @Nullable Entity entity, @Nullable Entity sourceEntity, int delay
+    ) {
+        BlockState blockState = this.getCachedState();
+        if (SculkSensorBlock.isInactive(blockState)) {
+            this.lastVibrationFrequency = SculkSensorBlock.FREQUENCIES.getInt(event);
+            SculkSensorBlock.setActive(/*entity, */world, this.pos, blockState, getPower(delay, listener.getRange()));
+        }
+
+    }
+}
