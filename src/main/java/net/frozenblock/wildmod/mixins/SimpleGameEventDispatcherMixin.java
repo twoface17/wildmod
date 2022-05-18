be8fd730741630cc771d3ac9a828c24de58427d1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-18 09:18:42 -0400

    Fix Test Fix

--- src/main/java/net/frozenblock/wildmod/mixins/SimpleGameEventDispatcherMixin.java
+++ src/main/java/net/frozenblock/wildmod/mixins/SimpleGameEventDispatcherMixin.java
@@ -1,6 +1,7 @@
 package net.frozenblock.wildmod.mixins;
 
 import net.frozenblock.wildmod.entity.WardenEntity;
+import net.frozenblock.wildmod.fromAccurateSculk.SculkTags;
 import net.frozenblock.wildmod.registry.RegisterEntities;
 import net.minecraft.block.SculkSensorBlock;
 import net.minecraft.entity.Entity;
@@ -8,6 +9,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -26,9 +28,10 @@ import java.util.Iterator;
 import java.util.List;
 
 @Mixin(SimpleGameEventDispatcher.class)
-public class SimpleGameEventDispatcherMixin{
+public class SimpleGameEventDispatcherMixin {
 
-    @Shadow private final World world;
+    @Shadow
+    private final World world;
 
     public SimpleGameEventDispatcherMixin(World world) {
         this.world = world;
@@ -38,79 +41,76 @@ public class SimpleGameEventDispatcherMixin{
     private void dispatch(GameEvent event, Entity entity, BlockPos pos, CallbackInfo ci) {
         BlockPos eventpos;
         World eventworld;
-        LivingEntity evententity;
+        Entity evententity;
 
-        if(SculkSensorBlock.FREQUENCIES.containsKey(event)) {
-            
-            if(entity == null) {
-                eventpos = pos;
-                eventworld = this.world;
-                evententity = null;
-            } else {
-                if(entity.getType() == EntityType.PLAYER) {
-                    if(event == GameEvent.STEP || event == GameEvent.HIT_GROUND || event == GameEvent.PROJECTILE_SHOOT) {
-                        if (entity.isSneaking()) {
+        if (entity == null) {
+            eventpos = pos;
+            eventworld = this.world;
+            evententity = null;
+        } else {
+            if (entity.getType() == EntityType.PLAYER) {
+                if (event == GameEvent.STEP || event == GameEvent.HIT_GROUND || event == GameEvent.PROJECTILE_SHOOT) {
+                    if (entity.isSneaking()) {
+                        eventpos = null;
+                        eventworld = null;
+                        evententity = null;
+                    } else {
+                        PlayerEntity player = (PlayerEntity) entity;
+                        Item booties = player.getEquippedStack(EquipmentSlot.FEET).getItem();
+                        Identifier identity = Registry.ITEM.getId(booties);
+                        if (
+                                new Identifier("wooledboots", "wooled_chainmail_boots").equals(identity) ||
+                                        new Identifier("wooledboots", "wooled_gold_boots").equals(identity) ||
+                                        new Identifier("wooledboots", "wooled_diamond_boots").equals(identity) ||
+                                        new Identifier("wooledboots", "wooled_netherite_boots").equals(identity) ||
+                                        new Identifier("wooledboots", "wooled_iron_boots").equals(identity)
+                        ) {
                             eventpos = null;
                             eventworld = null;
                             evententity = null;
                         } else {
-                            PlayerEntity player = (PlayerEntity) entity;
-                            Item booties = player.getEquippedStack(EquipmentSlot.FEET).getItem();
-                            Identifier identity = Registry.ITEM.getId(booties);
-                            if(
-                                    new Identifier("wooledboots", "wooled_chainmail_boots").equals(identity) ||
-                                            new Identifier("wooledboots", "wooled_gold_boots").equals(identity) ||
-                                            new Identifier("wooledboots", "wooled_diamond_boots").equals(identity) ||
-                                            new Identifier("wooledboots", "wooled_netherite_boots").equals(identity) ||
-                                            new Identifier("wooledboots", "wooled_iron_boots").equals(identity)
-
-                            ) {
-                                eventpos = null;
-                                eventworld = null;
-                                evententity = null;
-                            } else {
-                                eventpos = pos;
-                                eventworld = entity.getEntityWorld();
-                                evententity = (LivingEntity) entity;
-                            }
+                            eventpos = pos;
+                            eventworld = entity.getEntityWorld();
+                            evententity = entity;
                         }
-                    } else {
-                        eventpos = pos;
-                        eventworld = entity.getEntityWorld();
-                        evententity = (LivingEntity) entity;
-                    }
-                } else if(entity.getType() != RegisterEntities.WARDEN) {
-                    eventpos = pos;
-                    eventworld = entity.getEntityWorld();
-                    if(entity.isLiving()) {
-                        evententity = (LivingEntity) entity;
-                    } else {
-                        evententity = null;
                     }
                 } else {
-                    eventpos = null;
-                    eventworld = null;
+                    eventpos = pos;
+                    eventworld = entity.getEntityWorld();
+                    evententity = entity;
+                }
+            } else if (entity.getType() != RegisterEntities.WARDEN) {
+                eventpos = pos;
+                eventworld = entity.getEntityWorld();
+                if (entity.isLiving()) {
+                    evententity = entity;
+                } else {
                     evententity = null;
                 }
+            } else {
+                eventpos = null;
+                eventworld = null;
+                evententity = null;
             }
-            boolean bl1 = evententity != null || event==GameEvent.EAT || event==GameEvent.HIT_GROUND;
-            if(eventpos != null && eventworld != null && bl1) {
-                List<WardenEntity> wardens = this.world.getNonSpectatingEntities(WardenEntity.class, new Box(
-                        eventpos.getX() -18, eventpos.getY() -18, eventpos.getZ() -18,
-                        eventpos.getX() +18, eventpos.getY() +18, eventpos.getZ() +18)
-                );
-                Iterator<WardenEntity> var11 = wardens.iterator();
+        }
+        boolean bl1 = evententity != null || event == GameEvent.EAT || event == GameEvent.HIT_GROUND;
+        if (eventpos != null && eventworld != null && bl1) {
+            List<WardenEntity> wardens = this.world.getNonSpectatingEntities(WardenEntity.class, new Box(
+                    eventpos.getX() - 18, eventpos.getY() - 18, eventpos.getZ() - 18,
+                    eventpos.getX() + 18, eventpos.getY() + 18, eventpos.getZ() + 18)
+            );
+            Iterator<WardenEntity> var11 = wardens.iterator();
 
-                WardenEntity wardie;
-                while(var11.hasNext()) {
-                    wardie = var11.next();
-                    //System.out.println(Math.floor(Math.sqrt(wardie.getBlockPos().getSquaredDistance(eventpos, false))));
-                    if (wardie.getEntityWorld() == eventworld && Math.floor(Math.sqrt(wardie.getBlockPos().getSquaredDistance(eventpos))) <= 16) {
-                        boolean bl2 = event==GameEvent.HIT_GROUND && evententity==null;
-                        if (event!=GameEvent.PROJECTILE_LAND && event!=GameEvent.EAT && !bl2) {
-                            wardie.listen(eventpos, eventworld, evententity, wardie.eventSuspicionValue(event, evententity), null);
+            WardenEntity wardie;
+            while (var11.hasNext()) {
+                wardie = var11.next();
+                //System.out.println(Math.floor(Math.sqrt(wardie.getBlockPos().getSquaredDistance(eventpos, false))));
+                if (wardie.getEntityWorld() == eventworld && Math.floor(Math.sqrt(wardie.getBlockPos().getSquaredDistance(eventpos))) <= 16) {
+                    if (event.isIn(SculkTags.WARDEN_CAN_LISTEN)) {
+                        if (evententity instanceof ProjectileEntity projectile) {
+                            wardie.listen(eventpos, eventworld, projectile, projectile.getOwner(), 10, null);
                         } else {
-                            wardie.listen(eventpos, eventworld, null, 0, null);
+                            wardie.listen(eventpos, eventworld, evententity, null, 0, null);
                         }
                     }
                 }
