7b442919f6909a38c323a65ec3ed0c2e9de44b86
Merp <27739930+MerpisMe@users.noreply.github.com>
2022-05-24 11:05:09 -0500

    made frogs use temperate texture instead

--- src/main/java/net/frozenblock/wildmod/entity/render/frog/FrogEntityRenderer.java
+++ src/main/java/net/frozenblock/wildmod/entity/render/frog/FrogEntityRenderer.java
@@ -12,7 +12,7 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class FrogEntityRenderer extends MobEntityRenderer<FrogEntity, FrogEntityModel<FrogEntity>> {
 
-    private static final Identifier TEMPORARY_TEXTURE = new Identifier(WildMod.MOD_ID, "textures/entity/frog/cold_frog.png");
+    private static final Identifier TEMPORARY_TEXTURE = new Identifier(WildMod.MOD_ID, "textures/entity/frog/temperate_frog.png");
 
     public FrogEntityRenderer(EntityRendererFactory.Context context) {
         super(context, new FrogEntityModel<>(context.getPart(WildModClient.MODEL_FROG_LAYER)), 0.3F);
