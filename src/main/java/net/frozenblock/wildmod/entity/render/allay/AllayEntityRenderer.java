e01d29fb0ce9556db27be3759ff39ba67e2cc8f9
twoface17 <49493534+twoface17@users.noreply.github.com>
2022-09-23 16:10:26 -0600

    Add files via upload

---
+++ src/main/java/net/frozenblock/wildmod/entity/render/allay/AllayEntityRenderer.java
@@ -0,0 +1,32 @@
+package net.frozenblock.wildmod.entity.render.allay;
+
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.wildmod.WildMod;
+import net.frozenblock.wildmod.WildModClient;
+import net.frozenblock.wildmod.entity.AllayEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+
+@Environment(EnvType.CLIENT)
+public class AllayEntityRenderer extends MobEntityRenderer<AllayEntity, AllayEntityModel> {
+    private static final Identifier TEXTURE = new Identifier(WildMod.MOD_ID, "textures/entity/allay/allay.png");
+
+    public AllayEntityRenderer(EntityRendererFactory.Context context) {
+        super(context, new AllayEntityModel(context.getPart(WildModClient.MODEL_ALLAY_LAYER)), 0.4F);
+        //this.addFeature(new HeldItemFeatureRenderer<>(this, context.getHeldItemRenderer()));
+        this.addFeature(new AllayHeldItemFeatureRenderer(this));
+    }
+
+    public Identifier getTexture(AllayEntity allayEntity) {
+        return TEXTURE;
+    }
+
+    protected int getBlockLight(AllayEntity allayEntity, BlockPos blockPos) {
+        return 15;
+    }
+}
+
---
+++ src/main/java/net/frozenblock/wildmod/entity/render/allay/AllayHeldItemFeatureRenderer.java
@@ -0,0 +1,35 @@
+package net.frozenblock.wildmod.entity.render.allay;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.wildmod.entity.AllayEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3f;
+
+@Environment(EnvType.CLIENT)
+public class AllayHeldItemFeatureRenderer extends FeatureRenderer<AllayEntity, AllayEntityModel> {
+    public AllayHeldItemFeatureRenderer(FeatureRendererContext<AllayEntity, AllayEntityModel> featureRendererContext) {
+        super(featureRendererContext);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AllayEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+        ItemStack itemStack = entity.getEquippedStack(EquipmentSlot.MAINHAND);
+
+        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(-90.0F));
+        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+        matrices.translate(0.0, 0.0, -1.2);
+
+        matrices.push();
+
+        MinecraftClient.getInstance().getHeldItemRenderer().renderItem(entity, itemStack, ModelTransformation.Mode.GROUND, false, matrices, vertexConsumers, light);
+        matrices.pop();
+    }
+}
