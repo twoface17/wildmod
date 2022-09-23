dd3b83189c4e0a3196a2a50ce8e65d86e7792ab8
twoface17 <49493534+twoface17@users.noreply.github.com>
2022-09-23 16:16:53 -0600

    Add files via upload

---
+++ src/main/java/net/frozenblock/wildmod/misc/animation/Animation.java
@@ -0,0 +1,48 @@
+package net.frozenblock.wildmod.misc.animation;
+
+import com.google.common.collect.Maps;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.apache.commons.compress.utils.Lists;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Base GUI interface for handling callbacks related to
+ * keyboard or mouse actions.
+ * <p>
+ * Mouse coordinate is bounded by the size of the window in
+ * pixels.
+ */
+@Environment(EnvType.CLIENT)
+public record Animation(float lengthInSeconds, boolean looping, Map<String, List<Transformation>> boneAnimations) {
+    @Environment(EnvType.CLIENT)
+    public static class Builder {
+        private final float lengthInSeconds;
+        private final Map<String, List<Transformation>> transformations = Maps.newHashMap();
+        private boolean looping;
+
+        public static Animation.Builder create(float lengthInSeconds) {
+            return new Animation.Builder(lengthInSeconds);
+        }
+
+        private Builder(float lengthInSeconds) {
+            this.lengthInSeconds = lengthInSeconds;
+        }
+
+        public Animation.Builder looping() {
+            this.looping = true;
+            return this;
+        }
+
+        public Animation.Builder addBoneAnimation(String name, Transformation transformation) {
+            this.transformations.computeIfAbsent(name, namex -> Lists.newArrayList()).add(transformation);
+            return this;
+        }
+
+        public Animation build() {
+            return new Animation(this.lengthInSeconds, this.looping, this.transformations);
+        }
+    }
+}
